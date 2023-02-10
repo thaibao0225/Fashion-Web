af3ce1968f2b7542ca7145875d327135fb420613
DESKTOP-S25ODCE\Nhu <nhuvothiquynhn3@gmail.com>
2023-02-10 16:11:24 +0700

    Add project files.

---
+++ Fashion-Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Fashion_Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
